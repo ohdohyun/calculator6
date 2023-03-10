e8bde46bc8cd548ae6a99b98989cb92863373fc0
hoibi <kjmsgcg@gmail.com>
2023-03-10 20:19:42 +0900

    #20230310-BackendClass-GitTest/3 add1

---
+++ Add1.java
@@ -0,0 +1,13 @@
+public class Add1 {
+    int num1;
+    int num2;
+
+    public Add1(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int getAdd() {
+        return num1 + num2;
+    }
+}
